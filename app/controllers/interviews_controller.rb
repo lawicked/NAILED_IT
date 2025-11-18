dd4e18fd87f0236d5580e70dee506bb86dcd8414
viktor <victor.glesskrumhorn@gmail.com>
2025-11-18 22:24:06 +0800

    Conversation_view

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -4,4 +4,9 @@ class InterviewsController < ApplicationController
   def index
     @interviews = Interview.all
   end
+
+  #  def show
+  #   @interview = Interview.find(params[:id])
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     resources :conversations, only: [:create]
   end
 
+  resources :conversations, only: :show do
+    resources :messages, only: [:create]
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
