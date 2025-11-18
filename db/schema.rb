9717bf5ff2f162cb9fb02a47281e32153a7509b3
Lauren Wick <234356214+lawicked@users.noreply.github.com>
2025-11-18 11:46:52 +0800

    Role column added to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_18_033703) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_18_034501) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_18_033703) do
     t.bigint "conversation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
