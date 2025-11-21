8ede4712e4b1b0b51ae39881cb838481c3a66c39
viktor <victor.glesskrumhorn@gmail.com>
2025-11-21 12:59:29 +0800

    golden ratio fixing design

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '13': '3.25rem',  // 52px - Fibonacci-inspired
+        '21': '5.25rem',  // 84px - Fibonacci-inspired
+        '34': '8.5rem',   // 136px - Fibonacci-inspired
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
